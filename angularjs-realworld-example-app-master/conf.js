e10de0f794585f3b266af10b22fae5d8d4054014
gercidara.lira <gercidara.lira@grupoicts.com.br>
2019-08-17 05:20:25 -0500

    aaa

---
+++ angularjs-realworld-example-app-master/conf.js
@@ -0,0 +1,15 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['tests/test_protractor_spec.js'],
+
+    onPrepare: function(){
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath: './tests/reports/'
+        }))
+    },
+capabilities:{'browserName':'firefox'}
+  }
+  
